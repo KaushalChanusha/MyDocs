f94c74ef51c0809baabffc79394d01a4b1159ffc
KaushalChanusha <chanushakaushal1@gmail.com>
2019-05-10 07:22:35 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,217 @@
+package com.chanu.code.learn.arrays;
+
+public class Sample {
+
+//	100	
+//	Go to Data Check Context page
+//	101	
+//	Select the check box "No Issues"
+//	102	
+//	Click on the "Submit" button
+//	103	
+//	Click on Relay Result link
+//	104	
+//	Go to "Relay Results" page
+//	105	
+//	Click on Generate Document icon
+//	106	
+//	Click on "Add Item" link
+//	107	
+//	Select Template "Lilly Cares Enrollment Notification " from the drop down
+//	108	
+//	Click on "Generate Document" icon
+//	109	
+//	Click on "Submit" button
+//	110	
+//	Go to "Task Lists" section
+//	111	
+//	Click on Refresh button
+//	112	
+//	Select Action - Perform Task 1
+//	113	
+//	click the link Outbound task flow
+//	114	
+//	Go to Identify Work Party tap
+//	115	
+//	Select Fax number fro mthe drop down
+//	116	
+//	Go to select attachment tab
+//	117	
+//	Submit Task 1
+//	118	
+//	Click the link Fax flow 
+//	119	
+//	click on Submit
+//	120	
+//	Confirm Task 1
+//	121	
+//	Go to Action- Peform task 2
+//	122	
+//	Click the link outboud task flow 
+//	123	
+//	go to Identify work party tab
+//	124	
+//	Go to Select Attachent tab
+//	125	
+//	Submit Task 2
+//	126	
+//	Click on the link Postal Mail Flow
+//	127	
+//	click on Submit
+//	128	
+//	click on Confirm Task 2
+//	129	
+//	Click on Submit
+//	130	
+//	Click on "Submit" button on "Stage Specific Required Information" page when there is no missing informations
+//	131	
+//	Click Confirm on Resolved Confirmed page
+//	132	
+//	Log out from the system
+//		
+//	133	
+//	Click Login for Covance application
+//	134	
+//	Click the "Advance Entity Search"
+//	135	
+//	Select Client ID, Record Type
+//	136	
+//	Give the Record ID (previously created one ) and Search.
+//	137	
+//	Fill a field under search criteria
+//	138	
+//	Click on Entity 360 symbol
+//	139	
+//	Click Enrollment (s) Tab
+//	140	
+//	Click on  Create Order icon(Right hand side) for selected row
+//	141	
+//	Click on the Submit for confirm for order creation
+//	142	
+//	Click the  Arrow to expand the row
+//	143	
+//	Go to the "Order Details" Tab
+//	144	
+//	Go to the "Order Cases" panel and Click on "Manage Order"
+//	145	
+//	Page navigate to "Contextual Data Entry"
+//	146	
+//	verify whether "Service Classification" and "Product" not missing 
+//	147	
+//	click the Submit on Contex Data entry page
+//	148	
+//	Page navigate to "Missing Information" 
+//	149	
+//	click the Submit
+//	150	
+//	Click Order For Entry Details
+//	151	
+//	Click the "Prescription" tab 
+//	152	
+//	go to "Prescription Preferences" panel 
+//	153	
+//	Put a tick for "Received Letter of Medical Necessity?"
+//	154	
+//	Go to "Ship To" case and search entity
+//	155	
+//	Click the entity details and select the Prmary "Address'
+//	156	
+//	Go to "Shipping Address" and Select the "Shipping Address"
+//	157	
+//	Select "Shipment For" case as a "Undetermined"
+//	158	
+//	Go to "Order detail" tab 
+//	159	
+//	select "Prospective" as a Order Type
+//	160	
+//	Go to "Administration Details"  and select Place Of Service, Utilized Previously Requested Product
+//	161	
+//	Put a tick for "Received Proof of Previously Requested Product Utilized" and Submit.
+//	162	
+//	Page navigate to "Missing Information".
+//	163	
+//	Click on Submit.
+//	164	
+//	Page navigate to "View Potential Duplicates" and Submit.
+//	165	
+//	Page navigate to "Determine Eligibility".
+//	166	
+//	Put a "✔" in Check box, relevent to the eligibility one (appear in Green color) then Submit.
+//	167	
+//	Page navigate to "Missing Information".
+//	168	
+//	Click Link Determine Order Eligibility
+//	169	
+//	Go to Data Check Page
+//	170	
+//	Click No Issue Check box
+//	171	
+//	Click Submit button
+//	172	
+//	Select the "Overide"  & Submit.
+//	173	
+//	Click the link "Order Decision "
+//	174	
+//	Page navigate to "Make Decision" and Submit.
+//	175	
+//	Page navigate to "Missing Information".
+//	176	
+//	Select the "Overide"  & Submit.
+//	177	
+//	Click the "Note" tab 
+//	178	
+//	Add Summary in "Summary Note" and Submit.
+//	179	
+//	Page navigate to "Missing Information".
+//	180	
+//	Click the link "Request Fulfillment "
+//	181	
+//	Go to Data Check Page
+//	182	
+//	Click No Issue Check box
+//	183	
+//	Click Submit button
+//	184	
+//	Page navigate to "Order Request Fulfilment"
+//	185	
+//	Select the "Overide"  & Submit.
+//	186	
+//	Click the link RelayResults
+//	187	
+//	"Page navigate to ""Relay Results"
+//	188	
+//	Select the check boxes for "Have you correlated everything on this service request?" and "Confirm Tracking?"
+//	189	
+//	Page navigate to "Missing Information".
+//	190	
+//	Go to "Tracking Initial Data" page
+//	191	
+//	Go to "Select Order Tracking Group" section
+//	192	
+//	Confirm Order was Sent to Distributor ->Click on the arrow "Capture Details"
+//	193	
+//	Select a date from date picker to "Initial Date Sent to Distributor"
+//	194	
+//	Make the tracking options as "Complete" /Or "Pending"  
+//	195	
+//	Confirm Order was Received by the Distributor->->Click on the arrow "Capture Details"
+//	196	
+//	Select a date from date picker to "Date Received by the Distributor"
+//	197	
+//	Make the tracking options as "Complete" /Or "Pending"  
+//	198	
+//	Confirm Confirm Distributor Shipped the Order->->Click on the arrow "Capture Details"
+//	199	
+//	Select Has Order Shipped=Yes
+//	200	
+//	Make the tracking options as "Complete" /Or "Pending"  
+//	201	
+//	Click on "Submit" button
+//	202	
+//	Click on Submit button on "Stage Specific Required Information" page 
+//	203	
+//	Click Confirm on Resolved Confirmed page
+//	204	
+//	Log out from the system.
+
+}
